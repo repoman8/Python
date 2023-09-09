7943fa36e9575c2c0d41d7d63f2c7437fe5a0839
Taimarick Howard <46730114+repoman8@users.noreply.github.com>
2023-09-09 00:37:31 -0400

    Update custom-misp.py
    Hash filters were configured incorrectly by looking for a ipaddress variable which was not called during their elif statements therefore rendering them useless. Indentations were adjusted to negate that.

--- custom-misp.py
+++ custom-misp.py
@@ -34,9 +34,9 @@ alert_file.close()
 # New Alert Output if MISP Alert or Error calling the API
 alert_output = {}
 # MISP Server Base URL
-misp_base_url = "https://<host>/attributes/restSearch/"
+misp_base_url = "https://misp.cloud96.com/attributes/restSearch/"
 # MISP Server API AUTH KEY
-misp_api_auth_key = "<key>"
+misp_api_auth_key = "2uIwrLNOFH0qIR6Jw1n0utmpJoeINnXo8Vh1yk8Q"
 # API - HTTP Headers
 misp_apicall_headers = {"Content-Type":"application/json", "Authorization":f"{misp_api_auth_key}", "Accept":"application/json"}
 ## Extract Sysmon for Windows/Sysmon for Linux and Sysmon Event ID
@@ -220,30 +220,28 @@ elif event_source == 'ossec':
     if event_type == 'syscheck_entry_modified':
         try:
             wazuh_event_param = alert["syscheck"]["sha256_after"]
-            if ipaddress.ip_address(dst_ip).is_global:
-                wazuh_event_param = dst_ip
-                misp_search_value = "value:"f"{wazuh_event_param}"
-                misp_search_url = ''.join([misp_base_url, misp_search_value])
-                try:
-                    misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
-                except ConnectionError:
+            misp_search_value = "value:"f"{wazuh_event_param}"
+            misp_search_url = ''.join([misp_base_url, misp_search_value])
+            try:
+                misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
+            except ConnectionError:
+                alert_output["misp"] = {}
+                alert_output["integration"] = "misp"
+                alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                send_event(alert_output, alert["agent"])
+            else:
+                misp_api_response = misp_api_response.json()
+            # Check if response includes Attributes (IoCs)
+                if (misp_api_response["response"]["Attribute"]):
+            # Generate Alert Output from MISP Response
                     alert_output["misp"] = {}
-                    alert_output["integration"] = "misp"
-                    alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                    alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
+                    alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
+                    alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
+                    alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
                     send_event(alert_output, alert["agent"])
                 else:
-                    misp_api_response = misp_api_response.json()
-        # Check if response includes Attributes (IoCs)
-                    if (misp_api_response["response"]["Attribute"]):
-                # Generate Alert Output from MISP Response
-                        alert_output["misp"] = {}
-                        alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
-                        alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
-                        alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
-                        alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
-                        send_event(alert_output, alert["agent"])
-            else:
-                sys.exit()
+                    sys.exit()
         except IndexError:
             sys.exit()
     else:
@@ -252,30 +250,28 @@ elif event_source == 'ossec':
     if event_type == 'syscheck_entry_modified':
         try:
             wazuh_event_param = alert["syscheck"]["sha1_after"]
-            if ipaddress.ip_address(dst_ip).is_global:
-                wazuh_event_param = dst_ip
-                misp_search_value = "value:"f"{wazuh_event_param}"
-                misp_search_url = ''.join([misp_base_url, misp_search_value])
-                try:
-                    misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
-                except ConnectionError:
+            misp_search_value = "value:"f"{wazuh_event_param}"
+            misp_search_url = ''.join([misp_base_url, misp_search_value])
+            try:
+                misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
+            except ConnectionError:
+                alert_output["misp"] = {}
+                alert_output["integration"] = "misp"
+                alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                send_event(alert_output, alert["agent"])
+            else:
+                misp_api_response = misp_api_response.json()
+            # Check if response includes Attributes (IoCs)
+                if (misp_api_response["response"]["Attribute"]):
+            # Generate Alert Output from MISP Response
                     alert_output["misp"] = {}
-                    alert_output["integration"] = "misp"
-                    alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                    alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
+                    alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
+                    alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
+                    alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
                     send_event(alert_output, alert["agent"])
                 else:
-                    misp_api_response = misp_api_response.json()
-        # Check if response includes Attributes (IoCs)
-                    if (misp_api_response["response"]["Attribute"]):
-                # Generate Alert Output from MISP Response
-                        alert_output["misp"] = {}
-                        alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
-                        alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
-                        alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
-                        alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
-                        send_event(alert_output, alert["agent"])
-            else:
-                sys.exit()
+                    sys.exit()
         except IndexError:
             sys.exit()
     else:
@@ -284,30 +280,28 @@ elif event_source == 'ossec':
     if event_type == 'syscheck_entry_modified':
         try:
             wazuh_event_param = alert["syscheck"]["md5_after"]
-            if ipaddress.ip_address(dst_ip).is_global:
-                wazuh_event_param = dst_ip
-                misp_search_value = "value:"f"{wazuh_event_param}"
-                misp_search_url = ''.join([misp_base_url, misp_search_value])
-                try:
-                    misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
-                except ConnectionError:
+            misp_search_value = "value:"f"{wazuh_event_param}"
+            misp_search_url = ''.join([misp_base_url, misp_search_value])
+            try:
+                misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
+            except ConnectionError:
+                alert_output["misp"] = {}
+                alert_output["integration"] = "misp"
+                alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                send_event(alert_output, alert["agent"])
+            else:
+                misp_api_response = misp_api_response.json()
+            # Check if response includes Attributes (IoCs)
+                if (misp_api_response["response"]["Attribute"]):
+            # Generate Alert Output from MISP Response
                     alert_output["misp"] = {}
-                    alert_output["integration"] = "misp"
-                    alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                    alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
+                    alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
+                    alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
+                    alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
                     send_event(alert_output, alert["agent"])
                 else:
-                    misp_api_response = misp_api_response.json()
-        # Check if response includes Attributes (IoCs)
-                    if (misp_api_response["response"]["Attribute"]):
-                # Generate Alert Output from MISP Response
-                        alert_output["misp"] = {}
-                        alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
-                        alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
-                        alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
-                        alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
-                        send_event(alert_output, alert["agent"])
-            else:
-                sys.exit()
+                    sys.exit()
         except IndexError:
             sys.exit()
     else:
@@ -316,30 +310,28 @@ elif event_source == 'ossec':
     if event_type == 'syscheck_entry_added':
         try:
             wazuh_event_param = alert["syscheck"]["sha1_after"]
-            if ipaddress.ip_address(dst_ip).is_global:
-                wazuh_event_param = dst_ip
-                misp_search_value = "value:"f"{wazuh_event_param}"
-                misp_search_url = ''.join([misp_base_url, misp_search_value])
-                try:
-                    misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
-                except ConnectionError:
+            misp_search_value = "value:"f"{wazuh_event_param}"
+            misp_search_url = ''.join([misp_base_url, misp_search_value])
+            try:
+                misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
+            except ConnectionError:
+                alert_output["misp"] = {}
+                alert_output["integration"] = "misp"
+                alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                send_event(alert_output, alert["agent"])
+            else:
+                misp_api_response = misp_api_response.json()
+            # Check if response includes Attributes (IoCs)
+                if (misp_api_response["response"]["Attribute"]):
+            # Generate Alert Output from MISP Response
                     alert_output["misp"] = {}
-                    alert_output["integration"] = "misp"
-                    alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                    alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
+                    alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
+                    alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
+                    alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
                     send_event(alert_output, alert["agent"])
                 else:
-                    misp_api_response = misp_api_response.json()
-        # Check if response includes Attributes (IoCs)
-                    if (misp_api_response["response"]["Attribute"]):
-                # Generate Alert Output from MISP Response
-                        alert_output["misp"] = {}
-                        alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
-                        alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
-                        alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
-                        alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
-                        send_event(alert_output, alert["agent"])
-            else:
-                sys.exit()
+                    sys.exit()
         except IndexError:
             sys.exit()
     else:
@@ -348,30 +340,28 @@ elif event_source == 'ossec':
     if event_type == 'syscheck_entry_added':
         try:
             wazuh_event_param = alert["syscheck"]["md5_after"]
-            if ipaddress.ip_address(dst_ip).is_global:
-                wazuh_event_param = dst_ip
-                misp_search_value = "value:"f"{wazuh_event_param}"
-                misp_search_url = ''.join([misp_base_url, misp_search_value])
-                try:
-                    misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
-                except ConnectionError:
+            misp_search_value = "value:"f"{wazuh_event_param}"
+            misp_search_url = ''.join([misp_base_url, misp_search_value])
+            try:
+                misp_api_response = requests.get(misp_search_url, headers=misp_apicall_headers, verify=False)
+            except ConnectionError:
+                alert_output["misp"] = {}
+                alert_output["integration"] = "misp"
+                alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                send_event(alert_output, alert["agent"])
+            else:
+                misp_api_response = misp_api_response.json()
+            # Check if response includes Attributes (IoCs)
+                if (misp_api_response["response"]["Attribute"]):
+            # Generate Alert Output from MISP Response
                     alert_output["misp"] = {}
-                    alert_output["integration"] = "misp"
-                    alert_output["misp"]["error"] = 'Connection Error to MISP API'
+                    alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
+                    alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
+                    alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
+                    alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
                     send_event(alert_output, alert["agent"])
                 else:
-                    misp_api_response = misp_api_response.json()
-        # Check if response includes Attributes (IoCs)
-                    if (misp_api_response["response"]["Attribute"]):
-                # Generate Alert Output from MISP Response
-                        alert_output["misp"] = {}
-                        alert_output["misp"]["event_id"] = misp_api_response["response"]["Attribute"][0]["event_id"]
-                        alert_output["misp"]["category"] = misp_api_response["response"]["Attribute"][0]["category"]
-                        alert_output["misp"]["value"] = misp_api_response["response"]["Attribute"][0]["value"]
-                        alert_output["misp"]["type"] = misp_api_response["response"]["Attribute"][0]["type"]
-                        send_event(alert_output, alert["agent"])
-            else:
-                sys.exit()
+                    sys.exit()
         except IndexError:
             sys.exit()
     else:
